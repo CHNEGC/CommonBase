2e70fa98cb983025eae21dff2aea5ac8c8d77b01
Chen GuangChong <18889357323@163.com>
2022-03-24 11:44:20 +0800

    bug处理

--- lib_base/src/main/java/com/common/lib_base/util/LogUtil.kt
+++ lib_base/src/main/java/com/common/lib_base/util/LogUtil.kt
@@ -1,126 +1,118 @@
-package com.common.lib_base.util;
+package com.common.lib_base.util
 
-import android.util.Log;
-
-import com.mh.lib_base.BuildConfig;
+import android.util.Log
+import com.gyf.immersionbar.BuildConfig
 
 /**
  * Log工具类
  */
-public class LogUtil {
-
-    private LogUtil() {
-
-    }
-
-    public final static String TAG = "DEFAULT";
+object LogUtil {
+    private const val TAG = "DEFAULT"
 
     /**
      * 是否需要开启Log
      */
-    private final static boolean NEED_LOG = BuildConfig.DEBUG;
-
-    public static void i(String content) {
+    private const val NEED_LOG = BuildConfig.DEBUG
+    fun i(content: String) {
         if (NEED_LOG) {
-            Log.i(TAG, getLogPrefix() + content);
+            Log.i(TAG, logPrefix + content)
         }
     }
 
-    public static void i(String tag, String content) {
+    fun i(tag: String?, content: String) {
         if (NEED_LOG) {
-            Log.i(tag, getLogPrefix() + content);
+            Log.i(tag, logPrefix + content)
         }
     }
 
-    public static void d(String content) {
+    fun d(content: String) {
         if (NEED_LOG) {
-            Log.d(TAG, getLogPrefix() + content);
+            Log.d(TAG, logPrefix + content)
         }
     }
 
-    public static void d(String tag, String content) {
+    fun d(tag: String?, content: String) {
         if (NEED_LOG) {
-            Log.d(tag, getLogPrefix() + content);
+            Log.d(tag, logPrefix + content)
         }
     }
 
-    public static void e(String content) {
+    fun e(content: String) {
         if (NEED_LOG) {
-            Log.e(TAG, getLogPrefix() + content);
+            Log.e(TAG, logPrefix + content)
         }
     }
 
-    public static void e(String tag, String content) {
+    fun e(tag: String?, content: String) {
         if (NEED_LOG) {
-            Log.e(tag, getLogPrefix() + content);
+            Log.e(tag, logPrefix + content)
         }
     }
 
-    public static void v(String content) {
+    fun v(content: String) {
         if (NEED_LOG) {
-            Log.v(TAG, getLogPrefix() + content);
+            Log.v(TAG, logPrefix + content)
         }
     }
 
-    public static void v(String tag, String content) {
+    fun v(tag: String?, content: String) {
         if (NEED_LOG) {
-            Log.v(tag, getLogPrefix() + content);
+            Log.v(tag, logPrefix + content)
         }
     }
 
-    public static void w(String content) {
+    fun w(content: String) {
         if (NEED_LOG) {
-            Log.w(TAG, getLogPrefix() + content);
+            Log.w(TAG, logPrefix + content)
         }
     }
 
-    public static void w(String tag, String content) {
+    fun w(tag: String?, content: String) {
         if (NEED_LOG) {
-            Log.w(tag, getLogPrefix() + content);
+            Log.w(tag, logPrefix + content)
         }
     }
 
-    private static String getLogPrefix() {
-        return getClassName() + "(" + getLineNumber() + ")" + "$" + getMethodName() + ": ";
-    }
+    private val logPrefix: String
+        get() = "$className($lineNumber)$$methodName: "
 
     /**
      * 获取Log所在的类名 （getStackTrace的索引根据调用的顺序来决定，可通过打印Log栈来获取）
      */
-    private static String getClassName() {
-        try {
-            String classPath = Thread.currentThread().getStackTrace()[5].getClassName();
-            return classPath.substring(classPath.lastIndexOf(".") + 1);
-        } catch (Exception e) {
-            e.printStackTrace(System.out);
+    private val className: String?
+        get() {
+            try {
+                val classPath = Thread.currentThread().stackTrace[5].className
+                return classPath.substring(classPath.lastIndexOf(".") + 1)
+            } catch (e: Exception) {
+                e.printStackTrace(System.out)
+            }
+            return null
         }
-
-        return null;
-    }
 
     /**
      * 获取Log所在的方法名
      */
-    private static String getMethodName() {
-        try {
-            return Thread.currentThread().getStackTrace()[5].getMethodName();
-        } catch (Exception e) {
-            e.printStackTrace(System.out);
+    private val methodName: String?
+        get() {
+            try {
+                return Thread.currentThread().stackTrace[5].methodName
+            } catch (e: Exception) {
+                e.printStackTrace(System.out)
+            }
+            return null
         }
-
-        return null;
-    }
 
     /**
      * 获取Log所在的行
      */
-    private static int getLineNumber() {
-        try {
-            return Thread.currentThread().getStackTrace()[5].getLineNumber();
-        } catch (Exception e) {
-            e.printStackTrace(System.out);
+    private val lineNumber: Int
+        get() {
+            try {
+                return Thread.currentThread().stackTrace[5].lineNumber
+            } catch (e: Exception) {
+                e.printStackTrace(System.out)
+            }
+            return 0
         }
-
-        return 0;
-    }
 }
